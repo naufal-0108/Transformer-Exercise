6801333155b876c4e894097b64b28b97750fcf29
naufalfm <naufalfm0108@gmai.com>
2025-01-25 10:08:39 +0700

    modified model.py

--- model.py
+++ model.py
@@ -1,33 +1,107 @@
+import os, math
 import numpy as np
+from pyparsing import C
+from sympy import shape
 import tensorflow as tf
 from tokenizers import Tokenizer
 from tensorflow.keras.layers import Embedding, Dense
-from torch import dtype
+from torch import dtype, mode
 
+tf.random.set_seed(42)
 
-# q = Dense(8, activation='linear')
-# k = Dense(8, activation='linear')
+class EmbeddingLayer(tf.keras.layers.Layer):
 
-# q_out = q(embedding_layer(sentence_inputs_ind_encoded))
-# k_out = k(embedding_layer(sentence_inputs_ind_encoded))
+    def __init__(self, vocab_size, embedding_size, initializer=None, stddev=0.01, mean=0.0):
+        super(EmbeddingLayer, self).__init__()
+        self.vocab_size = vocab_size
+        self.embedding_size = embedding_size
+        self.stddev = stddev
+        self.mean = mean
+        self.initializer = initializer
+        if self.initializer is None:
+            self.initializer = tf.random_normal_initializer(mean=self.mean,
+                                                            stddev=self.stddev)
 
-# w = tf.matmul(q_out, k_out, transpose_b=True)
-# pad_mask = tf.cast(tf.math.equal(sentence_inputs_ind_encoded, 2), tf.float32)
-# casual_mask = tf.boolean_mask()
-# pad_mask = pad_mask[tf.newaxis, :]
-# print(pad_mask)
-# w_masked = w + (pad_mask * -1e9)
-# w_softmax = tf.nn.softmax(w_masked, axis=-1)
+    def build(self, input_shape):
 
-# print("Input ind:")
-# print(sentence_inputs_ind_encoded)
-# print(len(sentence_inputs_ind_encoded))
-# print("w:")
-# print(w)
-# print("w_masked:")
-# print(w_masked)
-# print("w_softmax:")
-# print(w_softmax)
+        with tf.name_scope("embedding_weights"):
+            self.embedding_weights = self.add_weight(
+                name="weights",
+                shape=(self.vocab_size, self.embedding_size),
+                dtype="float32",
+                initializer=self.initializer
+            )
+        super(EmbeddingLayer, self).build(input_shape)
+
+    def call(self, inputs, mode="embedding", scale=False):
+        if mode == "embedding":
+            return self.embedding(inputs, scale=scale)
+        elif mode == "projection":
+            return self.projection(inputs)
+        else:
+            raise ValueError("mode {} is not valid.".format(mode))
+
+    def embedding(self, inputs, scale=False):
+        with tf.name_scope("embedding"):
+            # Create binary mask of size [batch_size, length]
+            inputs = tf.cast(inputs, tf.int32)
+            embeddings = tf.nn.embedding_lookup(self.embedding_weights, inputs)
+
+            # Scale embedding by the sqrt of the hidden size
+            if scale:
+                embeddings *= self.embedding_size ** 0.5
+
+            return embeddings
+
+    def projection(self, inputs):
+        with tf.name_scope("output_layer"):
+            batch_size = tf.shape(inputs)[0]
+            seq_len = tf.shape(inputs)[1]
+
+            h_flat = tf.reshape(inputs, [-1, self.embedding_size])
+            logits = tf.matmul(h_flat, self.embedding_weights, transpose_b=True)
+
+            return tf.reshape(logits, [batch_size, seq_len, self.vocab_size])
+
+class Conv1D(tf.keras.layers.Layer):
+    def __init__(self, in_channels, out_channels, weights_init_stdev=0.02, weights_mean=0.0, bias_init=0.0):
+        super(Conv1D, self).__init__()
+
+        self.weights_init_stdev = weights_init_stdev
+        self.weights_mean = weights_mean
+        self.bias_init = bias_init
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+    def build(self, input_shape):
+        self.weight = self.add_weight(shape=[self.in_channels, self.out_channels],
+                                      dtype=tf.float32,
+                                      initializer=tf.random_normal_initializer(stddev=self.weights_init_stdev, mean=self.weights_mean))
+        self.bias = self.add_weight(shape=[self.out_channels], initializer=tf.constant_initializer(self.bias_init))
+        super(Conv1D, self).build(input_shape)
+
+    def call(self, inputs):
+        out_shape = [tf.shape(inputs)[0], tf.shape(inputs)[1]] + [self.out_channels]
+        inputs = tf.reshape(inputs, [-1, self.in_channels])
+        matmul_out = tf.matmul(inputs, self.weight) + self.bias
+        return tf.reshape(matmul_out, out_shape)
+    
+class FeedForward(tf.keras.layers.Layer):
+
+    def __init__(self, hidden_size, filter_size, activation=tf.nn.relu):
+        super(FeedForward, self).__init__()
+        self.hidden_size = hidden_size
+        self.filter_size = filter_size
+        self.activation = activation
+        self.dense_layer = Conv1D(self.hidden_size, self.filter_size)
+        self.output_dense_layer = Conv1D(self.filter_size, self.hidden_size)
+
+    def call(self, x):
+        output = self.dense_layer(x)
+        output = self.activation(output)
+        output = self.output_dense_layer(output)
+        return output
+
 
 class SinusoidalPositionalEncoding(tf.keras.layers.Layer):
     def __init__(self, d_model, max_len=1024):
@@ -62,9 +136,9 @@ class MultiHeadSelfAttention(tf.keras.layers.Layer):
         self.num_heads = num_heads
         self.dim_head = d_model // num_heads
         self.d_model = d_model
-        self.q = Dense(d_model, activation='linear')
-        self.k = Dense(d_model, activation='linear')
-        self.v = Dense(d_model, activation='linear')
+        self.q = Conv1D(d_model, d_model)
+        self.k = Conv1D(d_model, d_model)
+        self.v = Conv1D(d_model, d_model)
         self.dropout = tf.keras.layers.Dropout(dropout_rate)
         self.layer_norm = tf.keras.layers.LayerNormalization(epsilon=1e-6)
 
@@ -104,9 +178,9 @@ class MultiHeadCrossAttention(tf.keras.layers.Layer):
         self.num_heads = num_heads
         self.dim_head = q_dim // num_heads
 
-        self.q = Dense(q_dim, activation='linear')
-        self.k = Dense(k_dim, activation='linear')
-        self.v = Dense(k_dim, activation='linear')
+        self.q = Conv1D(q_dim, q_dim)
+        self.k = Conv1D(k_dim, k_dim)
+        self.v = Conv1D(k_dim, k_dim)
         self.dropout = tf.keras.layers.Dropout(dropout_rate)
         self.layer_norm = tf.keras.layers.LayerNormalization(epsilon=1e-6)
 
@@ -156,9 +230,9 @@ class MultiHeadCausalAttention(tf.keras.layers.Layer):
         super(MultiHeadCausalAttention, self).__init__()
         self.num_heads = num_heads
         self.dim_head = d_model // num_heads
-        self.q = Dense(d_model, activation='linear')
-        self.k = Dense(d_model, activation='linear')
-        self.v = Dense(d_model, activation='linear')
+        self.q = Conv1D(d_model, d_model)
+        self.k = Conv1D(d_model, d_model)
+        self.v = Conv1D(d_model, d_model)
         self.dropout = tf.keras.layers.Dropout(dropout_rate)
         self.layer_norm = tf.keras.layers.LayerNormalization(epsilon=1e-6)
 
@@ -189,10 +263,7 @@ class EncoderBlock(tf.keras.layers.Layer):
     def __init__(self, d_model, num_heads, n_dense=512, dropout_rate=0.1):
         super(EncoderBlock, self).__init__()
         self.mha = MultiHeadSelfAttention(d_model, num_heads)
-        self.mlp = tf.keras.Sequential([
-            Dense(n_dense, activation='relu'),
-            Dense(d_model)
-        ])
+        self.mlp = FeedForward(d_model, n_dense, activation=tf.nn.gelu)
         self.dropout = tf.keras.layers.Dropout(dropout_rate)
         self.layer_norm = tf.keras.layers.LayerNormalization(epsilon=1e-6)
     
@@ -212,10 +283,7 @@ class DecoderBlock(tf.keras.layers.Layer):
         super(DecoderBlock, self).__init__()
         self.causal_att = MultiHeadCausalAttention(q_dim, num_heads)
         self.cross_att = MultiHeadCrossAttention(q_dim, k_dim, num_heads)
-        self.mlp = tf.keras.Sequential([
-            Dense(n_dense, activation='relu'),
-            Dense(q_dim)
-        ])
+        self.mlp = FeedForward(q_dim, n_dense, activation=tf.nn.gelu)
         self.dropout = tf.keras.layers.Dropout(dropout_rate)
         self.layer_norm = tf.keras.layers.LayerNormalization(epsilon=1e-6)
     
@@ -250,17 +318,13 @@ class Transformer(tf.keras.layers.Layer):
     def __init__(self, num_layers, d_model, num_heads, n_dense, num_classes, dropout_rate=0.1):
         super(Transformer, self).__init__()
         self.blocks = [Block(d_model, d_model, num_heads, n_dense=n_dense, dropout_rate=dropout_rate) for _ in range(num_layers)]
-        self.mlp = tf.keras.Sequential([
-            Dense(n_dense, activation='relu'),
-            Dense(num_classes, activation='softmax')
-        ])
     
     def call(self, x_encoder, x_decoder, mask_encoder, mask_decoder):
 
         for block in self.blocks:
             x_encoder, x_decoder = block(x_encoder, x_decoder, mask_encoder, mask_decoder)
 
-        return self.mlp(x_decoder)
+        return x_encoder
 
 def transfomer_model(num_layers, d_model, num_heads, n_dense, num_vocabs_ind, num_vocabs_eng, max_len_ind, max_len_eng, dropout_rate=0.1):
 
@@ -269,17 +333,18 @@ def transfomer_model(num_layers, d_model, num_heads, n_dense, num_vocabs_ind, nu
     inputs_encoder_mask = tf.keras.Input(shape=(max_len_ind,), dtype=tf.float32)
     inputs_decoder_mask = tf.keras.Input(shape=(max_len_eng,), dtype=tf.float32)
 
-    embedding_layer_ind = Embedding(num_vocabs_ind, d_model)
-    embedding_layer_eng = Embedding(num_vocabs_eng, d_model)
+    embedding_layer_ind = EmbeddingLayer(num_vocabs_ind, d_model)
+    embedding_layer_eng = EmbeddingLayer(num_vocabs_eng, d_model)
 
     pos_encoding_ind = SinusoidalPositionalEncoding(d_model, max_len=max_len_ind)
     pos_encoding_eng = SinusoidalPositionalEncoding(d_model, max_len=max_len_eng)
 
-    x_encoder =  pos_encoding_ind(embedding_layer_ind(inputs_encoder))
+    x_encoder =  pos_encoding_ind(embedding_layer_ind(inputs_decoder))
     x_decoder =  pos_encoding_eng(embedding_layer_eng(inputs_decoder))
 
     transformer = Transformer(num_layers, d_model, num_heads, n_dense, num_classes=num_vocabs_eng, dropout_rate=dropout_rate)
     outputs = transformer(x_encoder, x_decoder, inputs_encoder_mask, inputs_decoder_mask)
+    outputs = embedding_layer_eng(outputs, mode="projection")
 
     model = tf.keras.Model(inputs=[inputs_encoder, inputs_decoder, inputs_encoder_mask, inputs_decoder_mask], outputs=outputs)
 
@@ -305,12 +370,12 @@ print(f"Input Eng: {sentence_inputs_eng_encoded}")
 print(f"Target: {sentence_target_encoded}")
 print()
 
-num_layers = 4
-d_model = 256
-num_heads = 8
+num_layers = 12
+d_model = 768
+num_heads = 12
 n_dense = 512
-num_vocabs_ind = 50000
-num_vocabs_eng = 50000
+num_vocabs_ind = 8192
+num_vocabs_eng = 8192
 max_len_ind = 128
 max_len_eng = 128
 dropout_rate = 0.1
@@ -324,5 +389,16 @@ transformers = transfomer_model(num_layers=num_layers,
                                 max_len_ind=max_len_ind,
                                 max_len_eng=max_len_eng,
                                 dropout_rate=dropout_rate)
+
+# embedding_layer = EmbeddingLayer(vocab_size=50000, embedding_size=8)
+# # print(embedding_layer.embedding_weights.shape)
+
+# embedding_out = embedding_layer(sentence_inputs_eng_encoded)
+# projection_out = embedding_layer(embedding_out, mode="projection")
+
+# print(embedding_layer.embedding_weights.shape)
+
+# print(embedding_out)
+# print(projection_out.shape)
 
 print(transformers.summary())
